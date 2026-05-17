07cd14108eb62eb452968c3834c3e04fde00cdd1
Omrajchera <omkarcherle137@gmail.com>
2026-05-17 10:01:55 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	
+	int a, b;
+	printf("Enter first number: ");
+	scanf("%d",&a);
+	
+	printf("Enter second number: ");
+	scanf("%d,&b");
+	
+	sum = a + b;
+	printf("Your addition is: ",sum);
+	 return 0;
+	
+	
+}
